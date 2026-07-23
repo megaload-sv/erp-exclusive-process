bce287369c0f559b7d47e7f127f8982f5515a879
José Luis Reyes <joseluis.r85@gmail.com>
2026-07-23 13:45:32 -0600

    Add TraceOps base UI component contract

---
+++ app/Libraries/TraceOps/UI/BaseComponent.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Libraries\TraceOps\UI;
+
+abstract class BaseComponent
+{
+    abstract public static function name(): string;
+
+    abstract public static function view(): string;
+
+    /**
+     * @return array<string, array<string, mixed>>
+     */
+    abstract public static function schema(): array;
+
+    /**
+     * @return array<string, mixed>
+     */
+    public static function metadata(): array
+    {
+        return [
+            'name' => static::name(),
+            'view' => static::view(),
+            'schema' => static::schema(),
+        ];
+    }
+
+    /**
+     * @param array<string, mixed> $props
+     * @return array<string, mixed>
+     */
+    final public static function normalize(array $props): array
+    {
+        return ComponentNormalizer::normalize($props, static::schema());
+    }
+}
