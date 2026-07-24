80d9e7a6fec69dd42e3bc66851743cbec3ba40bf
José Luis Reyes <joseluis.r85@gmail.com>
2026-07-23 19:04:16 -0600

    feat: add ERP developer console controller

---
+++ app/Controllers/DeveloperController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Libraries\TraceOps\UI\ComponentRegistry;
+use App\Libraries\TraceOps\UI\Components\ButtonComponent;
+
+final class DeveloperController extends BaseController
+{
+    public function index(): string
+    {
+        $registry = new ComponentRegistry([
+            ButtonComponent::class,
+        ]);
+
+        $descriptors = $registry->descriptors();
+        $slotCount = 0;
+        $propertyCount = 0;
+        $capabilityCount = 0;
+
+        foreach ($descriptors as $descriptor) {
+            $data = $descriptor->toArray();
+            $slotCount += count($data['slots']);
+            $propertyCount += count($data['properties']);
+            $capabilityCount += count($data['capabilities']);
+        }
+
+        return view('developer/index', array_merge($this->viewData, [
+            'title' => 'Developer Console',
+            'runtimeVersion' => $this->traceOps->version,
+            'descriptors' => $descriptors,
+            'runtimeStats' => [
+                'components' => $registry->count(),
+                'descriptors' => count($descriptors),
+                'properties' => $propertyCount,
+                'slots' => $slotCount,
+                'capabilities' => $capabilityCount,
+            ],
+            'runtimeHealth' => [
+                'Framework Core' => true,
+                'Universal Composition' => true,
+                'Named Slots' => true,
+                'Descriptor Engine' => true,
+            ],
+        ]));
+    }
+}
